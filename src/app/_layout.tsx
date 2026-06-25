16d190518856db8c87b6f01713e51d31cc6f879c
lioba00700 <lioba00700@gmail.com>
2026-06-25 23:36:30 +0900

    init: gluestack-ui 초기화

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,8 @@
 import { Stack } from "expo-router";
 
+import { GluestackUIProvider } from '@/components/ui/gluestack-ui-provider';
+import '@/example/src/global.css';
+
 export default function RootLayout() {
   return <Stack />;
 }
