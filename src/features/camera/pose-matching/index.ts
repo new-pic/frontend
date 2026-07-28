f1720de771546c504645fd175ae25bc991ca735a
lioba00700 <lioba00700@gmail.com>
2026-07-28 19:55:31 +0900

    feat(pose): 다중 인물 점수 계산과 피드백 구현

---
+++ src/features/camera/pose-matching/index.ts
@@ -0,0 +1,7 @@
+export * from "./lib/coordinate-transform";
+export * from "./lib/dwpose-pose-adapter";
+export * from "./lib/mediapipe-pose-adapter";
+export * from "./lib/pose-assignment";
+export * from "./lib/pose-geometry";
+export * from "./lib/pose-matcher";
+export * from "./model";
