e05f61dd17f472b517a73cb80af875a11e19a396
lioba00700 <lioba00700@gmail.com>
2026-08-14 00:46:42 +0900

    feat: 버그 제보 폼 링크 수정

--- src/shared/constants/external-links.ts
+++ src/shared/constants/external-links.ts
@@ -1,5 +1,6 @@
 export const EXTERNAL_LINKS = {
-  BUG_REPORT: "https://forms.gle/Cr5erBoRrnHyayRw8",
+  BUG_REPORT:
+    "https://working-skunk-fbd.notion.site/3bbfdee90e2f80c283ebc87a0dafdac7?v=3bbfdee90e2f80f3a25d000c5d24d4a3",
   TERMS_OF_SERVICE:
     "https://working-skunk-fbd.notion.site/newpic-3affdee90e2f807385e3ded6fe4ac37a",
 } as const;
